e25822f702853f5fe04d5203ee0831a6396af1dd
KeitetsuWorks <KeitetsuWorks@users.noreply.github.com>
2021-09-19 19:55:39 +0900

    Google Adsenseの広告の読込み処理を変更

--- js/common.js
+++ js/common.js
@@ -3,3 +3,13 @@ function showNowYear(){
     var year = now.getFullYear();
     document.write(year);
 }
+
+document.addEventListener('mdl-componentupgraded', function(){
+    if (!document.getElementById('adsbygooglejs')) {
+        var head = document.getElementsByTagName('head')[0];
+        var script = document.createElement('script');
+        script.id = 'adsbygooglejs';
+        script.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+        head.appendChild(script);
+    }
+});
